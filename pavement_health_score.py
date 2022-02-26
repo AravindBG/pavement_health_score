27697bce652a03a0dcf2c396d0d8923ac9085415
Aravind B <aravindb@qburst.com>
2022-02-26 13:19:53 +0530

    Parsing the csv file

--- pavement_health_score.py
+++ pavement_health_score.py
@@ -1,19 +1,50 @@
+from email import header
 import sys
 import os
+import csv
 
-# Read from the input arguments
+
+def validate_file_ext(exten):
+    if exten.lowercase() == "csv":
+        return True
+    else:
+        return False
+
+
+def read_csv(path):
+    # Open CSV
+    # file = open(path)
+    # print(type(file))
+    # Read from the CSV file
+    with open(path, encoding='utf-8') as file:
+        csvreader = csv.reader(file)
+        # Extract the field/header names
+        headers = []
+        header = next(csvreader)
+        print(header)
+
+        # Extract rows/records
+        rows = []
+        for row in csvreader:
+            rows.append(row)
+        print(rows)
+        # file.close()
+
+
+    # Read from the input arguments
 if len(sys.argv) == 2:
     # Path of the file
-    print(len(sys.argv))
     path = sys.argv[1]
     # Validate the file name
     filename, file_extension = os.path.splitext(path)
     print(file_extension)
+    if validate_file_ext:
+        read_csv(filename+file_extension)
+    else:
+        raise Exception('Not a CSV file')
 
 else:
     raise Exception('No valid arguments found')
-
-    # Read from the CSV file
 
     # Calculate the health score
 
