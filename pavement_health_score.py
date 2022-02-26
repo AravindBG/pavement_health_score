2ebe27b6130fa8f10c60d7cf0bcbcb5ba1c3eb4b
Aravind B <aravindb@qburst.com>
2022-02-26 12:48:33 +0530

    Read the arguments from the command

--- pavement_health_score.py
+++ pavement_health_score.py
@@ -1,12 +1,20 @@
-
+import sys
+import os
 
 # Read from the input arguments
+if len(sys.argv) == 2:
+    # Path of the file
+    print(len(sys.argv))
+    path = sys.argv[1]
+    # Validate the file name
+    filename, file_extension = os.path.splitext(path)
+    print(file_extension)
 
+else:
+    raise Exception('No valid arguments found')
 
-# Read from the CSV file
+    # Read from the CSV file
 
+    # Calculate the health score
 
-# Calculate the health score
-
-
-# Write these value to a file and save in a separate folder
+    # Write these value to a file and save in a separate folder
