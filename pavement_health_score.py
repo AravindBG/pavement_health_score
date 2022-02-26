05bd86ad8e333760ff4f8e933fce439a5f41e37f
Aravind B <aravindb@qburst.com>
2022-02-26 22:26:09 +0530

    Complete the result csv implementation, health score calculations. Git ignore file update. Pending: Input validations and testing

--- pavement_health_score.py
+++ pavement_health_score.py
@@ -1,7 +1,14 @@
-from email import header
 import sys
 import os
 import csv
+
+PCI_CONSTANT = 1
+BBD_CONSTANT = 1
+IRI_CONSTANT = 1
+
+directory = os.path.join(os.getcwd(), 'Results')
+if not os.path.isdir(directory):
+    os.mkdir(directory)
 
 
 def validate_file_ext(exten):
@@ -10,24 +17,67 @@ def validate_file_ext(exten):
     else:
         return False
 
+# Write these value to a file and save in a separate folder
 
-def read_csv(path):
+
+def create_csv_file(file_name, headers, rows):
+    dest_file_name = "Result_"+file_name
+
+    filePath = os.path.join(directory, dest_file_name)
+    print("Result file path ", filePath)
+    fp = open(filePath, 'w')
+    # create the csv writer
+    writer = csv.writer(fp)
+    # print("Type ", type(headers))
+    # print("Headers ", headers)
+    writer.writerow(headers)
+    # write a row to the csv file
+    # print("Rows ", rows)
+    writer.writerows(rows)
+    fp.close()
+
+# Calculate the health score
+
+
+def calculate_values(row_values):
+    pci = float(row_values[1])
+    bbd = float(row_values[2])
+    iri = float(row_values[3])
+
+    if((0 <= pci <= 100) and (0 <= bbd <= 10) and (0 <= iri <= 10)):
+        result = (pci * PCI_CONSTANT) + \
+            (bbd * BBD_CONSTANT) + (iri * IRI_CONSTANT)
+        # Format the result to 3 decimal points
+        formatted_result = round(result, 3)
+        row_values.append(str(formatted_result))
+        return row_values
+    else:
+        return None
+
+
+def read_csv(file_name):
+    print("Processing file", file_name)
     try:
         # Open csv
-        file = open(path, encoding='cp1252')
+        file = open(file_name, encoding='cp1252')
         # Read from the csv file
         csvreader = csv.reader(file)
         # Extract the field/header names
         headers = []
-        header = next(csvreader)
-        print(header)
+        headers = next(csvreader)
+        headers.append("Result")
+        # print(headers)
 
         # Extract rows/records
         rows = []
         for row in csvreader:
-            rows.append(row)
-        print(rows)
-
+            if calculate_values(row) == None:
+                print("Invalid input values")
+            else:
+                updated_row = calculate_values(row)
+                rows.append(updated_row)
+        # print(rows)
+        create_csv_file(path, headers, rows)
     except:
         raise Exception(
             'Unable to open the file. Check the encoding of the csv file')
@@ -41,7 +91,7 @@ if len(sys.argv) == 2:
     path = sys.argv[1]
     # Validate the file name
     filename, file_extension = os.path.splitext(path)
-    print(file_extension)
+    # print(file_extension)
     if validate_file_ext:
         read_csv(filename+file_extension)
     else:
@@ -49,7 +99,3 @@ if len(sys.argv) == 2:
 
 else:
     raise Exception('No valid arguments found')
-
-    # Calculate the health score
-
-    # Write these value to a file and save in a separate folder
